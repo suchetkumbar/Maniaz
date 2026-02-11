1497cd7d52a89196124c247bcdd4f0949057c2d5
BrownieByte <suchetkumbar@gmail.com>
2026-02-11 11:48:43 +0530

    Implemented the websocket server

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm';
-import { pg } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-if(!process.env.DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in environment variables');
 }
 
-export const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
